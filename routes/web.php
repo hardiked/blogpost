3aba3ab4be7dacb341c4043fb79dc8bf0f586447
hardik modi <hmodi97122@yahoo.in>
2017-07-22 21:37:53 +0530

    project completed

---
+++ routes/web.php
@@ -0,0 +1,79 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::group(['middleware'=>['web']],function(){
+	Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+	Route::post('/signup',[
+			'uses'=>'UserController@postSignUp',
+			'as'=>'signup'
+		]);
+
+	Route::post('/signin',[
+			'uses'=>'UserController@postSignIn',
+			'as'=>'signin'
+		]);
+
+	Route::get('/logout',[
+			'uses'=>'UserController@getLogOut',
+			'as'=>'logout'
+		]);
+
+	Route::get('/dashboard',[
+			'uses'=>'PostController@getDashboard',
+			'as'=>'dashboard',
+		])->middleware('auth');
+
+	Route::post('/createpost',[
+			'uses'=>'PostController@postCreatePost',
+			'as'=>'post.create'
+		])->middleware('auth');
+
+	Route::get('/delete-pst/{post_id}',[
+			'uses'=>'PostController@getDeletePost',
+			'as'=>'post.delete'
+		])->middleware('auth');	
+	/*checking whether request receoved or not
+	Route::post('/edit',function(\Illuminate\Http\Request $request){
+		//return response()->json(['message'=> $request['postId']]);
+
+	})->name('edit');*/			
+
+	Route::post('/edit',[
+			'uses' => 'PostController@postEditPost',
+			'as' => 'edit'
+		]);
+
+	Route::get('/account',[
+			'uses' => 'UserController@getAccount',
+			'as' => 'account'
+		]);
+
+	Route::post('/updateaccount',[
+			'uses' => 'UserController@postSaveAccount',
+			'as' => 'account.save'
+		]);
+
+	Route::get('/userimage/{filename}',[
+			'uses' => 'UserController@getUserImage',
+			'as' => 'account.image'
+		]);
+
+	Route::post('/like',[
+			'uses' => 'PostController@postLikePost',
+			'as' => 'like'
+		]);
+});
